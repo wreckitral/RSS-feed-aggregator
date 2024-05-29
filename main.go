a7052bb92c276f421fab3f973ee74130c4d92e9c
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-05-29 21:49:03 +0700

    chore(main): create server instance

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+    if err := godotenv.Load(".env"); err != nil {
+        log.Fatal(err)
+    }
+
+    port := ":" + os.Getenv("PORT")
+
+    server := NewAPIServer(port)
+
+    if err := server.Run(); err != nil {
+        log.Fatal(err)
+    }
+}
