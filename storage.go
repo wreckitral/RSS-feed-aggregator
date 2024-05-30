355a88db10401bd4fe4081b06ae580a0640bbdb8
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-05-30 18:56:32 +0700

    feat(storage): add create user to db function

--- storage.go
+++ storage.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"os"
 
 	_ "github.com/lib/pq"
 	"github.com/wreckitral/RSS-feed-aggregator/internal/database"
 )
+
+type Storage interface {
+    CreateUserToDb(*User) error
+}
 
 type PostgresStore struct {
     DB *database.Queries
@@ -14,6 +20,7 @@ type PostgresStore struct {
 
 func NewPostgresStore() (*PostgresStore, error) {
     dbConn := os.Getenv("DBCONN")
+    fmt.Println(dbConn)
 
     db, err := sql.Open("postgres", dbConn)
     if err != nil {
@@ -31,3 +38,18 @@ func NewPostgresStore() (*PostgresStore, error) {
     }, nil
 }
 
+func (s *PostgresStore) CreateUserToDb(user *User) error {
+    userToDb := database.CreateUserParams{
+        ID: user.ID,
+        CreatedAt: user.CreatedAt,
+        UpdatedAt: user.UpdatedAt,
+        Name: user.Name,
+    }
+
+    _, err := s.DB.CreateUser(context.Background(), userToDb) 
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
