9202f96e0c7dfbdabead1fccf5d622023c8e04f1
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-05-30 17:57:37 +0700

    feat(types): create user struct and new account func

--- types.go
+++ types.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+    ID uuid.UUID `json:"id"`
+    CreatedAt time.Time `json:"createdAt"`
+    UpdatedAt time.Time `json:"updatedAt"`
+    Name string `json:"name"`
+}
+
+func NewUser(name string) (*User, error) {
+    id := uuid.New()
+    
+    return &User{
+        ID: id,
+        CreatedAt: time.Now().UTC(),
+        UpdatedAt: time.Now().UTC(),
+        Name: name,
+    }, nil
+}
