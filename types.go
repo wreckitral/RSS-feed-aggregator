c62b282e560d30406dc146862f9a2250960c40e5
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-01 13:00:39 +0700

    feat(types): create new feed struct
    create new struct for request and response of endpoint creating feed

--- types.go
+++ types.go
@@ -19,6 +19,20 @@ type CreateUserRequest struct {
     Name string `json:"name"`
 }
 
+type FeedResponse struct {
+    ID uuid.UUID `json:"id"`
+    CreatedAt time.Time `json:"createdAt"`
+    UpdatedAt time.Time `json:"updatedAt"`
+    Name string `json:"name"`
+    Url string `json:"url"`
+    UserID uuid.UUID `json:"userId"`
+}
+
+type FeedRequest struct {
+    Name string `json:"name"`
+    Url string `json:"url"`
+}
+
 func NewUser(name string) (*database.User, error) {
     id := uuid.New()
     
@@ -27,5 +41,18 @@ func NewUser(name string) (*database.User, error) {
         CreatedAt: time.Now(),
         UpdatedAt: time.Now(),
         Name: name,
+    }, nil
+}
+
+func NewFeed(name, url string, userId uuid.UUID) (*database.Feed, error) {
+    id := uuid.New()
+
+    return &database.Feed{
+        ID: id,
+        CreatedAt: time.Now(),
+        UpdatedAt: time.Now(),
+        Name: name,
+        Url: url,
+        UserID: userId,
     }, nil
 }
