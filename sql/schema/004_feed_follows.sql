a10ee6f7807c315dee4eb6c4aec2e53ab4134f50
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-02 21:10:53 +0700

    chore: create and generate sql files for feed follows

---
+++ sql/schema/004_feed_follows.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+CREATE TABLE feed_follows (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id UUID NOT NULL,
+    feed_id UUID NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_feed
+        FOREIGN KEY(feed_id)
+        REFERENCES feeds(id)
+        ON DELETE CASCADE,
+    unique(user_id, feed_id)
+);
+
+-- +goose Down
+DROP TABLE feed_follows;
