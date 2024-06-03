118ea2019e13f3236b657c3c02fcf7d21f684301
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-03 22:38:09 +0700

    feat(rss): create url to feed struct

--- rss.go
+++ rss.go
@@ -1,18 +1,51 @@
 package main
 
+import (
+	"encoding/xml"
+	"io"
+	"net/http"
+	"time"
+)
+
 type RSSFeed struct {
     Channel struct {
-        Title string `xml:"title"`
-        Link string `xml:"link"`
+        Title       string `xml:"title"`
+        Link        string `xml:"link"`
         Description string `xml:"description"`
-        Language string `xml:"language"`
-        Item []RSSItem `xml:"item"`
+        Language    string `xml:"language"`
+        Item        []RSSItem `xml:"item"`
     } `xml:"channel"`
 }
 
 type RSSItem struct {
-    Title string `xml:"title"`
-    Link string `xml:"link"`
+    Title       string `xml:"title"`
+    Link        string `xml:"link"`
     Description string `xml:"description"`
-    PubDate string `xml:"pubDate"`
+    PubDate     string `xml:"pubDate"`
+}
+
+func urlToFeed(url string) (*RSSFeed, error) {
+    httpClient := http.Client {
+        Timeout: 10 * time.Second,
+    }
+
+    resp, err := httpClient.Get(url)
+    if err != nil {
+        return nil, err
+    }
+
+    defer resp.Body.Close()
+
+    data, err := io.ReadAll(resp.Body)
+    if err != nil {
+        return nil, err
+    }
+
+    rssFeed := RSSFeed{}
+
+    if err := xml.Unmarshal(data, &rssFeed); err != nil {
+        return nil, err
+    }
+
+    return &rssFeed, nil
 }
