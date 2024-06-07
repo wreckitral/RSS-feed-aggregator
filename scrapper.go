f2e9ba24eedd3158d18ed940e1a02e2017cadf32
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-07 16:02:00 +0700

    feat(scrapper): add logic to create posts table

--- scrapper.go
+++ scrapper.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/wreckitral/RSS-feed-aggregator/internal/database"
 )
 
@@ -47,7 +51,56 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
     }
 
     for _, item := range rssFeed.Channel.Item {
-        log.Println("Found post:", item.Title, "on feed", feed.Name)
+        desc := sql.NullString{}
+        if item.Description != "" {
+            desc.String = item.Description
+            desc.Valid = true
+        }
+
+        pubAt, err := parseTime(item.PubDate)
+        if err != nil {
+            log.Println("error parsing time:", err, "on item", item.Title)
+        }
+
+        _, err = db.CreatePost(context.Background(), 
+            database.CreatePostParams{
+                ID: uuid.New(),
+                CreatedAt: time.Now().UTC(),
+                UpdatedAt: time.Now().UTC(),
+                Title: item.Title,
+                Url: item.Link,
+                Description: desc,
+                PublishedAt: pubAt,
+                FeedID: feed.ID,
+            },
+        )
+        if err != nil {
+            if strings.Contains(err.Error(), "duplicate key") {
+                continue
+            }
+            log.Println("failed to create new post:", err)
+        }
     }
+
     log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
+}
+
+func parseTime(timeStr string) (time.Time, error) {
+    // Primary format to try
+    formats := []string{
+        time.RFC1123Z,
+        time.RFC1123,
+        time.RFC3339,
+    }
+
+    var t time.Time
+    var err error
+
+    for _, format := range formats {
+        t, err = time.Parse(format, timeStr)
+        if err == nil {
+            return t, nil
+        }
+    }
+    return time.Time{}, fmt.Errorf("unable to parse time: %v", timeStr)
 }
